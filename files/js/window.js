1c6084395b0288339feb9040d42802366c0522ee
Mrbysco <Mrbysco@users.noreply.github.com>
2020-05-22 22:07:15 +0200

    Make IE resizable
    fix some bugs

--- files/js/window.js
+++ files/js/window.js
@@ -1,1 +1,62 @@
-$(document).ready(function(){$("#notepad").resizable(),$("#notepad").draggable({start:function(){$("#notepad").css("z-index","2"),$("#msn").css("z-index","0")}}),$("#msn").resizable(),$("#msn").draggable({start:function(){$("#msn").css("z-index","2"),$("#notepad").css("z-index","0")}})});
+var ids = ["#notepad", "#msn", "#internet"];
+
+$(document).ready(function () {
+    $("#notepad").resizable(),
+    $("#notepad").draggable({
+        start: function () {
+            focus("#notepad");
+        },
+        handle: "#notepad-header",
+    }),
+    $("#msn").resizable(),
+    $("#msn").draggable({
+        start: function () {
+            focus("#msn");
+        },
+        handle: "#msn-header",
+    });
+    $("#internet").resizable(),
+    $("#internet").draggable({
+        start: function () {
+            focus("#internet");
+        },
+        handle: "#internet-header",
+    });
+    $("#internet").draggable('disable');
+
+    $("#notepad").click(function(){
+        focus("#notepad");
+    });
+    $("#msn").click(function(){
+        focus("#msn");
+    });
+    $("#internet").click(function(){
+        focus("#internet");
+    });
+});
+
+function toggleFullscreen(str) {
+    var window = $(str);
+    window.toggleClass("fullscreen");
+    if(window.hasClass("fullscreen")) {
+        window.draggable('disable');
+        window.resizable('disable');
+    } else {
+        window.draggable('enable');
+        window.resizable('enable');
+    }
+}
+
+function focus(str) {
+    $(str).css("z-index", "2");
+    unfocus(str);
+}
+
+function unfocus(str) {
+    for(i = 0; i < ids.length; i++) {
+        var id = ids[i];
+        if(id !== str) {
+            $(id).css("z-index", "0")
+        };
+    }
+}
